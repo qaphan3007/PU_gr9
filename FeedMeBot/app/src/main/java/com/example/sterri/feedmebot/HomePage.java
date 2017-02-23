7a9e89b7430bee8461a3d3075b836839c3eef004
kv1402 <kieuvanbuithi@hotmail.com>
2017-02-23 16:17:54 +0100

    impl. logout button

--- FeedMeBot/app/src/main/java/com/example/sterri/feedmebot/HomePage.java
+++ FeedMeBot/app/src/main/java/com/example/sterri/feedmebot/HomePage.java
@@ -4,13 +4,43 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class HomePage extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomePage extends AppCompatActivity implements View.OnClickListener {
+
+    //firebase auth object
+    private FirebaseAuth firebaseAuth;
+    //create button View
+    private Button buttonLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        //initializing firebase authentication object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //if the user is not logged in
+        //that means current user will return null
+        if(firebaseAuth.getCurrentUser() == null){
+            //closing this activity
+            finish();
+            //starting login activity
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
+        //getting current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        //init button view
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+
+        //adding listener to button
+        buttonLogout.setOnClickListener(this);
     }
     public void goToJavaOversikt(View view) {
 
@@ -19,4 +49,24 @@ public class HomePage extends AppCompatActivity {
 
 
     }
+
+    public void onClick(View view) {
+        //if logout is pressed
+        if(view == buttonLogout){
+            //logging out the user
+            firebaseAuth.signOut();
+            //closing activity
+            finish();
+            //starting login activity
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
 }
+
+
+
+
+
+
+
+
