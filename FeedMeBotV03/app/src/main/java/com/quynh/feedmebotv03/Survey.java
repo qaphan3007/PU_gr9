e1f891d21579c28fd12eb9c35e383f1517bae3cf
Jakob <sterrip@gmail.com>
2017-03-07 11:54:22 +0100

    Rejected commit: Default

--- FeedMeBotV03/app/src/main/java/com/quynh/feedmebotv03/Survey.java
+++ FeedMeBotV03/app/src/main/java/com/quynh/feedmebotv03/Survey.java
@@ -2,13 +2,20 @@ package com.quynh.feedmebotv03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
 // Survey class is a common class with questions for all assignments in all subjects
 public class Survey extends AppCompatActivity {
-
+    final String TAG = "test1233";
+    TextView textViewTest;
     TextView textView;
-    SeekBar seekBar;
+    TextView textViewHour;
+    SeekBar seekBarHour;
     SeekBar seekBarDifficultLevel;
     Assignment assignment; // create an assignment object@
 
@@ -16,20 +23,22 @@ public class Survey extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
-        textView = (TextView) (findViewById(R.id.textViewHour));
-
-        seekBar = (SeekBar) findViewById(R.id.seekBarHours);
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        final Assignment assignment = new Assignment();
+        textViewHour = (TextView) (findViewById(R.id.textViewHour));
+        seekBarHour = (SeekBar) findViewById(R.id.seekBarHours);
+        seekBarHour.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 if (progress == 0) {
-                    textView.setText("Jeg kokte øvingen......");
-                    assignment.setAssignmentHours(0);// sets the amount of time used in assignment object to 0
+                    textViewHour.setText("Jeg kokte øvingen......"); // prints how many hours.
+                    assignment.setAssignmentHours(progress); // Sets the the hours spent to the assignment object
+                    if(assignment.getAssignmentHours() == 0) {
+                        Toast.makeText(Survey.this, "Det funker!!!!! Og vi er awesome", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    textView.setText(progress + " hours");
-                    assignment.setAssignmentHours(progress); // sets the amount of time used in assignment object
+                    textViewHour.setText(progress + " hours"); // prints how many hours.
+                    assignment.setAssignmentHours(progress); // Sets the the hours spent to the assignment object
                 }
-
             }
 
             @Override
@@ -42,12 +51,30 @@ public class Survey extends AppCompatActivity {
 
             }
         });
-
+        textView= (TextView) findViewById(R.id.textViewA2);
         seekBarDifficultLevel = (SeekBar) findViewById(R.id.seekBarDifficultLevel);
         seekBarDifficultLevel.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
+                if (progress==0) {
+                    textView.setText("Very easy"); // prints level in textView
+                    assignment.setAssignmentLevel("Very easy"); // Sets the difficultylevel to the assignment object
+                } else if  (progress==1) {
+                    textView.setText("Easy");
+                    assignment.setAssignmentLevel("Easy"); // Sets the difficultylevel to the assignment object
+                }else if  (progress==2) {
+                    textView.setText("Medium");
+                    assignment.setAssignmentLevel("Medium"); // Sets the difficultylevel to the assignment object
+                }else if  (progress==3) {
+                    textView.setText("OK");
+                    assignment.setAssignmentLevel("OK"); // Sets the difficultylevel to the assignment object
+                }else if  (progress==4) {
+                    textView.setText("Challenging");
+                    assignment.setAssignmentLevel("Challenging"); // Sets the difficultylevel to the assignment object
+                }else if  (progress==5) {
+                    textView.setText("Very hard");
+                    assignment.setAssignmentLevel("I give up"); // Sets the difficultylevel to the assignment object
+                }
             }
 
             @Override
